384214eec61975225d70a892e523f476a5afcf85
HoangTu <le.hoang.tu@sun-asterisk.com>
2020-07-17 14:41:09 +0700

    add admin list order

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -12,6 +12,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="keywords" content="Visitors Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <script type="application/x-javascript">
         addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); }
     </script>
@@ -26,6 +27,9 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
     <link href="{{asset('admin/css/font-awesome.css')}}" rel="stylesheet">
     <!-- //font-awesome icons -->
     <script src="{{asset('admin/js/jquery2.0.3.min.js')}}"></script>
+    <!--web font -->
+    <link rel="stylesheet" href="{{asset('admin/css/toastr.min.css')}}">
+
 </head>
 
 <body>
@@ -41,12 +45,18 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 
     </section>
     <script src="{{asset('admin/js/bootstrap.js')}}"></script>
+    <!-- //pre-process -->
+
+    <script src="{{asset('client/js/toastr.min.js')}}" ></script>
     <script src="{{mix('admin/js/jquery.dcjqaccordion.2.7.js')}}"></script>
     <script src="{{mix('admin/js/scripts.js')}}"></script>
     <script src="{{mix('admin/js/jquery.slimscroll.js')}}"></script>
     <script src="{{mix('admin/js/jquery.nicescroll.js')}}"></script>
     <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="js/flot-chart/excanvas.min.js"></script><![endif]-->
     <script src="{{mix('admin/js/jquery.scrollTo.js')}}"></script>
+    {{-- custom js --}}
+    <script src="{{mix('admin/js/orders.js')}}"></script>
+    {{-- //custom js --}}
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,12 @@ Route::group(['prefix'=>'admin-page'],function(){
     Route::get('user/edit/{id?}', 'Admin\UsersController@edit')->name('edit_users');
     Route::post('user/update/{id?}', 'Admin\UsersController@update')->name('update_users');
     Route::get('user/delete/{id?}', 'Admin\UsersController@destroy')->name('delete_users');
+    Route::get('/order', 'Admin\OrdersController@index')->name('manage_orders');
+    Route::get('order/show/{id?}', 'Admin\OrdersController@show')->name('show_orders');
+    Route::post('/order/accept_order', 'Admin\OrdersController@acceptOrder')->name('accept_orders');
+    Route::get('/order/update_quantity/{id?}', 'Admin\OrdersController@updateQuantity')->name('update_quantity');
+    Route::post('order/denied_order', 'Admin\OrdersController@deniedOrder')->name('denied_orders');
+
 });
 
 /* Admin */
@@ -58,3 +64,22 @@ Route::get('users/{user}/detail', 'UserController@show')->name('users.detail');
 Route::patch('users/{user}', 'UserController@update')->name('users.update');
 Route::get('/changePassword','UserController@showChangePassword');
 Route::post('/changePassword', 'UserController@changePassword')->name('changePassword');
+
+Route::group(['prefix' => 'products'], function(){
+    Route::get('/product-accord-category/{category}', 'ProductController@product_accord_category')->name('pro_accord_cate');
+    Route::get('/product-accord-price/{parameter}', 'ProductController@product_accord_price')->name('pro_accord_price');
+    Route::get('/{product}', 'ProductController@show')->name('product_detail');
+});
+
+Route::group(['prefix' => 'order'], function(){
+    Route::post('/order-item', 'OrderController@orderItem')->name('order_item');
+    Route::post('/order-item/update-quantity', 'OrderController@updateQuantity')->name('update_quantity');
+    Route::post('/order-item/delete-item', 'OrderController@deleteItem')->name('delete_item');
+    Route::get('/order-item/{order}', 'OrderController@checkout')->name('checkout');
+    Route::post('/order-item/update_ship_address', 'OrderController@update_ship_address')->name('update_ship_address');
+    Route::get('/order-item/finish-order/{order}', 'OrderController@finish_order')->name('finish_order');
+    Route::post('/order-item/accept_order', 'OrderController@accept_order')->name('accept_order');
+    Route::get('/list-order/{user}', 'OrderController@list_order')->name('list_order');
+    Route::get('/list-order/{order}/detail', 'OrderController@order_detail')->name('order_detail');
+});
+/* Client*/
